8c9d3dab2ec86dbcc7cc8353da7c2ec38988733b
user1 <user1@localhost.localdomain>
2019-04-23 10:07:49 +0300

    Finish user edit, update, index and destroy action

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+   def setup
+      @user = users(:example)
+    end  
+
    test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,6 +12,22 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+   end
+
+   test "header links" do
+      get root_path
+      assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", users_path, count: 0
+      assert_select "a[href=?]", edit_user_path(@user), count: 0
+      assert_select "a[href=?]", logout_path, count: 0
+      assert_select "a[href=?]", user_path(@user), count: 0
+      log_in_as(@user)
+      get root_path
+      assert_select "a[href=?]", login_path, count: 0
+      assert_select "a[href=?]", users_path
+      assert_select "a[href=?]", edit_user_path(@user)
+      assert_select "a[href=?]", logout_path
+      assert_select "a[href=?]", user_path(@user)
    end
 
    test "get contact" do
